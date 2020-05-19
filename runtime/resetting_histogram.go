1e10634f833b09e2efd48c16a1e62aa05131a2a1
Anton Evangelatov <anton.evangelatov@gmail.com>
2020-05-19 12:14:46 +0200

    remove Int64Slice type, because sort.Slice
    Co-authored-by: Cory Schwartz <cory@protocol.ai>

--- runtime/resetting_histogram.go
+++ runtime/resetting_histogram.go
@@ -139,7 +139,7 @@ func (t *StandardResettingHistogram) Snapshot() Histogram {
 	currentValues := t.values
 	t.values = make([]int64, 0, InitialResettingHistogramSliceCap)
 
-	sort.Sort(Int64Slice(currentValues))
+	sort.Slice(currentValues, func(i, j int) bool { return currentValues[i] < currentValues[j] })
 
 	return &ResettingHistogramSnapshot{
 		values: currentValues,
@@ -296,10 +296,3 @@ func (t *ResettingHistogramSnapshot) calc(percentiles []float64) {
 
 	t.calculated = true
 }
-
-// Int64Slice attaches the methods of sort.Interface to []int64, sorting in increasing order.
-type Int64Slice []int64
-
-func (s Int64Slice) Len() int           { return len(s) }
-func (s Int64Slice) Less(i, j int) bool { return s[i] < s[j] }
-func (s Int64Slice) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
