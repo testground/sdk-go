f66cc16be9ba1fbbb0ec268583575ec7d0dd0219
Anton Evangelatov <anton.evangelatov@gmail.com>
2020-10-05 13:58:59 +0200

    move notification to client

--- go.mod
+++ go.mod
@@ -8,15 +8,10 @@ require (
 	github.com/go-redis/redis/v7 v7.4.0
 	github.com/hashicorp/go-multierror v1.1.0
 	github.com/influxdata/influxdb1-client v0.0.0-20200515024757-02f0bf5dbca3
-	github.com/mitchellh/mapstructure v1.3.3
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_golang v1.7.1
 	github.com/rcrowley/go-metrics v0.0.0-20200313005456-10cdbea86bc0
 	github.com/stretchr/testify v1.5.1
+	github.com/testground/testground v0.5.3
 	go.uber.org/zap v1.15.0
-	golang.org/x/net v0.0.0-20191112182307-2180aed22343 // indirect
 	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a
-	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
-	golang.org/x/tools v0.0.0-20191125144606-a911d9008d1f // indirect
-	google.golang.org/protobuf v1.25.0 // indirect
 )
---
+++ sync/client_watch.go
@@ -0,0 +1,74 @@
+package sync
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/go-redis/redis/v7"
+	"github.com/testground/sdk-go/runtime"
+	"github.com/testground/testground/pkg/logging"
+	"go.uber.org/zap"
+)
+
+type WatchClient struct {
+	ctx     context.Context
+	rclient *redis.Client
+	log     *zap.SugaredLogger
+}
+
+func NewWatchClient(ctx context.Context) (*WatchClient, error) {
+	log := logging.S()
+	rclient, err := redisClient(ctx, log)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create redis client: %w", err)
+	}
+
+	return &WatchClient{
+		ctx,
+		rclient,
+		log,
+	}, nil
+}
+
+func (w *WatchClient) FetchAllEvents(rp *runtime.RunParams) ([]*runtime.Event, error) {
+	key := fmt.Sprintf("run:%s:plan:%s:case:%s", rp.TestRun, rp.TestPlan, rp.TestCase)
+
+	var events []*runtime.Event
+
+	id := "0"
+	for {
+		args := &redis.XReadArgs{}
+		args.Streams = []string{key, id}
+		args.Block = 1 * time.Second
+		args.Count = 10000
+
+		streams, err := w.rclient.XRead(args).Result()
+		if err != nil {
+			if err == redis.Nil {
+				break
+			}
+
+			return nil, err
+		}
+
+		for _, xr := range streams {
+			for _, msg := range xr.Messages {
+				payload := msg.Values[RedisPayloadKey].(string)
+
+				ev := &runtime.Event{}
+				err := json.Unmarshal([]byte(payload), ev)
+				if err != nil {
+					panic(err)
+				}
+
+				events = append(events, ev)
+
+				id = msg.ID
+			}
+		}
+	}
+
+	return events, nil
+}
